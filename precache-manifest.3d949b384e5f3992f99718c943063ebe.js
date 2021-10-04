9f4251de9f3895181d41eecc225f6c4d3dfee9a9
shady ahmed <shadiex@hotmail.com>
2021-10-04 22:09:47 +0200

    Updates

---
+++ precache-manifest.3d949b384e5f3992f99718c943063ebe.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9cf57315dd8002dc8da8461f8a0dcdc",
+    "url": "/fortune-homepage/index.html"
+  },
+  {
+    "revision": "566d71ee3797a76b9c8b",
+    "url": "/fortune-homepage/static/css/main.1d9353c3.chunk.css"
+  },
+  {
+    "revision": "fc69426d10411248424f",
+    "url": "/fortune-homepage/static/js/2.5358e026.chunk.js"
+  },
+  {
+    "revision": "846e3710ca5ee3f911296f05447fe08c",
+    "url": "/fortune-homepage/static/js/2.5358e026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566d71ee3797a76b9c8b",
+    "url": "/fortune-homepage/static/js/main.71590544.chunk.js"
+  },
+  {
+    "revision": "2a179826dfde1fad7aff",
+    "url": "/fortune-homepage/static/js/runtime-main.a056395d.js"
+  },
+  {
+    "revision": "19cb2c08166e1e47004f5c06b5a0a1e0",
+    "url": "/fortune-homepage/static/media/facebook.19cb2c08.svg"
+  },
+  {
+    "revision": "b43588b63655847ebc7c08d0370a9b2a",
+    "url": "/fortune-homepage/static/media/facebookfooter.b43588b6.svg"
+  },
+  {
+    "revision": "f9a62a67c3511307db975be4d829905d",
+    "url": "/fortune-homepage/static/media/heart.f9a62a67.svg"
+  },
+  {
+    "revision": "44782c39cbcbb6fcab91ad506c6b7f60",
+    "url": "/fortune-homepage/static/media/instagramfooter.44782c39.svg"
+  },
+  {
+    "revision": "f42f52bc8f549fc0cf4dfb8e98ab51d7",
+    "url": "/fortune-homepage/static/media/search.f42f52bc.svg"
+  },
+  {
+    "revision": "406e30dfbc053094c77ebc0bf8610a35",
+    "url": "/fortune-homepage/static/media/shopper.406e30df.svg"
+  },
+  {
+    "revision": "d727e08cd93175a936e50c08676d6db4",
+    "url": "/fortune-homepage/static/media/shopperred.d727e08c.svg"
+  },
+  {
+    "revision": "7a23a65664795f557dec77a4500244fc",
+    "url": "/fortune-homepage/static/media/signs.7a23a656.svg"
+  },
+  {
+    "revision": "40e9e9dd0ae0ae3ef7cb60b58f911916",
+    "url": "/fortune-homepage/static/media/ui.40e9e9dd.svg"
+  },
+  {
+    "revision": "086d14a0d79fa521cce111fd789cf473",
+    "url": "/fortune-homepage/static/media/user.086d14a0.svg"
+  }
+]);
