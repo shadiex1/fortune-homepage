e23292072b185693abab32a8ea415334b1836424
shady ahmed <shadiex@hotmail.com>
2021-10-04 23:18:52 +0200

    modified Category Img

--- src/Components/3/navDropdown/navDropdown.jsx
+++ src/Components/3/navDropdown/navDropdown.jsx
@@ -37,7 +37,7 @@ const navDropdown = (props) => (
       </div>
 
       <div className={styles.categoryImg}>
-        <img src={ process.env.PUBLIC_URL+"/images/fortune/missingImages/image69.png"} alt="category Image" />
+        <img src={ process.env.PUBLIC_URL+"/images/fortune/missingImages/Image69.png"} alt="category Image" />
       </div>
     </div>
   </React.Fragment>
